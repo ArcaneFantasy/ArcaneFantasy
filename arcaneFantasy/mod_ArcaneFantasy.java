48a4586d36fdacb26d9cfdf9b5e9861190dee287
Scorch855 <scorch267@gmail.com>
2012-09-03 12:21:39 +1000

    better setting out

--- arcaneFantasy/mod_ArcaneFantasy.java
+++ arcaneFantasy/mod_ArcaneFantasy.java
@@ -14,9 +14,10 @@ public class mod_ArcaneFantasy extends NetworkMod  {
 	public String getVersion() { return "Arcane Fantasy Beta 0.1"; }
 
 	
-//                      Type      String Name                 Type                   ID         Class                            Extra Info  
-	public static final Item      Stringname =         new ItemItemType            (1201, Example.example             , 5, 0).setItemName("ItemName");	
-	public static final Block
+//                      Type      String Name                 Type                       ID                 Extra Info  
+	public static final Item      ExampleItem       =         new ItemExampleItem      (1200);
+	public static final Block     UnNamedOre        =         new BlockUnnamedOre      (1201);
+	public static final Block     ExampleBlock      =         new BlockExampleBlock    (1202)            .setLightValue(1F);
 	
 @Override	
 public void load() {
@@ -29,11 +30,13 @@ ArcaneFantasyConfig.InitliazeConfig("ArcaneFantasy");
 // Register Block      Block Name
 ModLoader.registerBlock(ExampleBlock);
 
-// Add Names      String Name              Ingame Name
-ModLoader.addName(Stringname,             "NameOfObject");
-		
+// Add Names      String Name               Ingame Name
+ModLoader.addName(UnNamedOre,              "Unnamed ore");
+ModLoader.addName(ExampleBlock,            "This is an example block");
+ModLoader.addName(ExampleItem,             "This is an example item");
+
 // Add Recipies
-ModLoader.addRecipe(new ItemStack(Stringname, 1), new Object []{ " + ",   " X ",  "   ",   '+', Item.example, 'X',Block.example           });
-ModLoader.addRecipe(new ItemStack(Stringname, 1), new Object []{ " + ",   " X ",  "   ",   '+', Item.example, 'X',Block.example           });		
+ModLoader.addRecipe(new ItemStack(ExampleBlock, 1), new Object []{ " + ",   " X ",  "   ",   '+', Item.example, 'X',Block.example           });
+ModLoader.addRecipe(new ItemStack(ExampleItem , 1), new Object []{ " + ",   " X ",  "   ",   '+', Item.example, 'X',Block.example           });		
 }
 }
