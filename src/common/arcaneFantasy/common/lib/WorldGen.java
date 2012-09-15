8babf43bd1c71520522c6eb630e67c5c6208f6da
HMPerson1 <shengzhang@Sheng-Zhangs-iMac.local>
2012-09-14 23:48:42 -0400

    Revert "Revert "Nerfed world-gen, optimized generation a bit, Updated zip""
    This reverts commit 1181e3b762b5684a24517bce9809da0cd809c583.

--- src/common/arcaneFantasy/common/lib/WorldGen.java
+++ src/common/arcaneFantasy/common/lib/WorldGen.java
@@ -19,30 +19,30 @@ public class WorldGen {
     public static enum Ore {
 
         // TODO: More fine-tuning
-        CRYSTAL(20, 0, 128, null, 10, 1, NON_GEN_LEVELS),
-        AMETHYST(8, 0, 16, null, 2, 1, NON_GEN_LEVELS),
-        COPPER(8, 0, 64, null, 15, 1, NON_GEN_LEVELS),
-        PYRITE(8, 0, 32, null, 5, 1, NON_GEN_LEVELS),
-        MAGNEZE(8, 0, 32, null, 3, 1, NON_GEN_LEVELS),
-        SILVER(8, 0, 32, null, 3, 1, NON_GEN_LEVELS),
-        MAGILITH(8, 0, 32, null, 3, 1, NON_GEN_LEVELS),
-        DRAGON(50, 0, 16, null, 1, 0.05, NON_GEN_LEVELS),
-        PLATINUM(8, 0, 16, null, 3, 1, NON_GEN_LEVELS),
-        PALADINIUM(8, 0, 16, null, 3, 1, NON_GEN_LEVELS),
-        FIRIUM(8, 0, 16, null, 3, 0.2, NON_GEN_LEVELS),
-        GLACIUM(8, 0, 16, ICY_BIOMES, 3, 0.2, NON_GEN_LEVELS),
-        FOUDIUM(8, 0, 16, null, 3, 0.2, NON_GEN_LEVELS),
-        SLATE(50, 64, 128, null, 20, 0.3, NON_GEN_LEVELS),
-        SALT(50, 32, 64, null, 20, 0.45, NON_GEN_LEVELS),
-        MARBLE(100, 16, 64, null, 20, 0.2, NON_GEN_LEVELS),
-        MAGNETITE(20, 0, 64, null, 20, 0.5, NON_GEN_LEVELS),
-        CHALK(50, 32, 64, null, 20, 0.3, NON_GEN_LEVELS);
+        CRYSTAL(15, 0, 128, null, 10, 0.2, NON_GEN_LEVELS),
+        AMETHYST(7, 0, 16, null, 2, 0.2, NON_GEN_LEVELS),
+        COPPER(8, 0, 64, null, 15, 0.2, NON_GEN_LEVELS),
+        PYRITE(8, 0, 32, null, 3, 0.2, NON_GEN_LEVELS),
+        MAGNEZE(8, 0, 32, null, 3, 0.2, NON_GEN_LEVELS),
+        SILVER(8, 0, 32, null, 2, 0.2, NON_GEN_LEVELS),
+        MAGILITH(8, 0, 32, null, 3, 0.2, NON_GEN_LEVELS),
+        DRAGON(150, 0, 16, null, 1, 0.005, NON_GEN_LEVELS),
+        PLATINUM(8, 0, 16, null, 2, 0.2, NON_GEN_LEVELS),
+        PALADINIUM(8, 0, 16, null, 3, 0.2, NON_GEN_LEVELS),
+        FIRIUM(7, 0, 16, null, 3, 0.04, NON_GEN_LEVELS),
+        GLACIUM(7, 0, 16, ICY_BIOMES, 3, 0.04, NON_GEN_LEVELS),
+        FOUDRIUM(7, 0, 16, null, 3, 0.04, NON_GEN_LEVELS),
+        SLATE(50, 64, 128, null, 20, 0.06, NON_GEN_LEVELS),
+        SALT(50, 32, 64, null, 20, 0.09, NON_GEN_LEVELS),
+        MARBLE(100, 16, 64, null, 20, 0.04, NON_GEN_LEVELS),
+        MAGNETITE(20, 0, 64, null, 20, 0.1, NON_GEN_LEVELS),
+        CHALK(50, 32, 64, null, 20, 0.06, NON_GEN_LEVELS);
         public static final Ore[] GEMS = {
             CRYSTAL, AMETHYST
         };
         public static final Ore[] METALS = {
             COPPER, PYRITE, MAGNEZE, SILVER, MAGILITH, DRAGON,
-            PLATINUM, PALADINIUM, FIRIUM, GLACIUM, FOUDIUM
+            PLATINUM, PALADINIUM, FIRIUM, GLACIUM, FOUDRIUM
         };
         public static final Ore[] STONES = {
             SLATE, SALT, MARBLE, MAGNETITE, CHALK
